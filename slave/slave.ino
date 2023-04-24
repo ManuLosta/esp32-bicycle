19920f39efb98bd4f26ffb362c398106da77965b
ManuLosta <mlostalom@gmail.com>
2023-04-24 20:45:05 -0300

    ESP-NOW connection

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,47 +1,111 @@
 #include <PulseSensorPlayground.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
+uint8_t broadcastAddress[] = {0xC8, 0xF0, 0x9E, 0xF8, 0x9E, 0xC0};
+
+typedef struct struct_message {
+  int bpm;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+/*
+   The following hardware timer setup supports ESP32
+*/
 hw_timer_t * sampleTimer = NULL;
 portMUX_TYPE sampleTimerMux = portMUX_INITIALIZER_UNLOCKED;
 
 PulseSensorPlayground pulseSensor;
 
 const int PULSE_INPUT = 36;
-const int PULSE_BLINK = 13;    
-const int PULSE_FADE = 5;
-const int THRESHOLD = 685; 
+const int PULSE_BLINK = 2;
+const int THRESHOLD = 520; 
 
+/*
+    This is the interrupt service routine.
+    We need to declare it after the PulseSensor Playground
+    library is compiled, so that the onSampleTime
+    function is known.
+*/
 void IRAM_ATTR onSampleTime() {
   portENTER_CRITICAL_ISR(&sampleTimerMux);
   PulseSensorPlayground::OurThis->onSampleTime();
   portEXIT_CRITICAL_ISR(&sampleTimerMux);
 }
 
-boolean sendPulseSignal = false;
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void setup() {
   Serial.begin(115200);
 
+  /*
+   ESP32 analogRead defaults to 13 bit resolution
+   PulseSensor Playground library works with 10 bit
+  */
   analogReadResolution(10);
 
   pulseSensor.analogInput(PULSE_INPUT);
   pulseSensor.blinkOnPulse(PULSE_BLINK);
-  pulseSensor.fadeOnPulse(PULSE_FADE);
   pulseSensor.setSerial(Serial);
   pulseSensor.setThreshold(THRESHOLD);
 
+  /*
+    This will set up and start the timer interrupt on ESP32.
+    The interrupt will occur every 2000uS or 500Hz.
+  */
   sampleTimer = timerBegin(0, 80, true);                
   timerAttachInterrupt(sampleTimer, &onSampleTime, true);  
   timerAlarmWrite(sampleTimer, 2000, true);      
   timerAlarmEnable(sampleTimer);
 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop() {
-  int myBPM = pulseSensor.getBeatsPerMinute();  
+  myData.bpm = pulseSensor.getBeatsPerMinute();  
 
   if (pulseSensor.sawStartOfBeat()) {
-   Serial.println(String("BPM: ") + myBPM);                        
+   Serial.println(String("BPM: ") + myData.bpm);     
+   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+
   }
-  delay(500);  
+  delay(20);  
 }
 
