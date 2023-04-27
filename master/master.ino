b851dddaa5cea79631940295fac25f0488b2039c
ManuLosta <mlostalom@gmail.com>
2023-04-27 09:24:38 -0300

    OLED code in master ESP32

--- master/master.ino
+++ master/master.ino
@@ -1,5 +1,14 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define i2c_Address 0x3c
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 typedef struct struct_message {
   int bpm;
@@ -8,6 +17,11 @@ typedef struct struct_message {
 // Create a struct_message called myData
 struct_message myData;
 
+const unsigned char Heart_Icon [] PROGMEM = {
+  0x00, 0x00, 0x18, 0x30, 0x3c, 0x78, 0x7e, 0xfc, 0xff, 0xfe, 0xff, 0xfe, 0xee, 0xee, 0xd5, 0x56, 
+  0x7b, 0xbc, 0x3f, 0xf8, 0x1f, 0xf0, 0x0f, 0xe0, 0x07, 0xc0, 0x03, 0x80, 0x01, 0x00, 0x00, 0x00
+};
+
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
@@ -15,6 +29,14 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(len);
   Serial.print("BPM: ");
   Serial.println(String(myData.bpm));
+
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);
+  display.setTextSize(2);
+   display.drawBitmap(6, 0, Heart_Icon, 16, 16, SH110X_WHITE);
+  display.setCursor(22, 0);
+  display.println(String(myData.bpm));
+  display.display();  
 }
 
 void setup() {
@@ -33,6 +55,12 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
+
+  display.begin(i2c_Address, true); // Address 0x3C default
+  delay(2000);
+  display.display();
+  display.clearDisplay();
+  display.display(); 
 }
 
 void loop() {
